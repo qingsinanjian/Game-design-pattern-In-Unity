48d13bc7920967b7b22aff5808edcf057f867b7e
DESKTOP-6FGN5NP\Egoist <13260963605wjx@gmail.com>
2024-03-13 15:20:55 +0800

    完成事件系统

---
+++ Assets/Scripts/GameEventSystem/Observer/NewStageObserver/NewStageObserverArchievement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewStageObserverArchievement : IGameEventObserver
+{
+    private NewStageSubject mSubject;
+    private ArchievementSystem mArchSystem;
+    public NewStageObserverArchievement(ArchievementSystem mArchSystem)
+    {
+        this.mArchSystem = mArchSystem;
+    }
+    public override void SetSubject(IGameEventSubject subject)
+    {
+        mSubject = subject as NewStageSubject;
+    }
+
+    public override void Update()
+    {
+        mArchSystem.SetMaxStageLv(mSubject.stageCount);
+    }
+}
+
---
+++ Assets/Scripts/GameEventSystem/Observer/SoldierKilledObserver/SoldierKilledObserverArchievement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierKilledObserverArchievement : IGameEventObserver
+{
+    private ArchievementSystem mArchSystem;
+
+    public SoldierKilledObserverArchievement(ArchievementSystem mArchSystem)
+    {
+        this.mArchSystem = mArchSystem;
+    }
+
+    public override void SetSubject(IGameEventSubject subject)
+    {
+        return;
+    }
+
+    public override void Update()
+    {
+        mArchSystem.AddSoldierKilledCount();
+    }
+}
+
